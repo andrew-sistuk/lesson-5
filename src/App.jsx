c920aa2f024aa73babbb7d5acc814a4142ce0213
Wolf <andresistuk@gmail.com>
2024-06-02 15:36:03 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { Header } from 'components';
+import { Home, Rates } from 'pages';
+
+import { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+export const App = () => {
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      const crd = pos.coords;
+
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
+};
